54475cbf9d46845f145b672b051e3e77db6fcd03
Carlos <contact@carloscolin.me>
2021-07-28 23:37:20 -0500

    Added CoinDetailScreen

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class CoinDetailScreen extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <View>
+                    <Text>Coin Detail Screen</Text>
+                </View>
+            </React.Fragment>
+        );
+    }
+}
+
+export default CoinDetailScreen;
