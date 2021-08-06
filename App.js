72bf2c31267c6e60e6fd6403730397b8ae571416
Carlos <contact@carloscolin.me>
2021-08-05 20:14:19 -0500

    Added bottom tab navigation

--- App.js
+++ App.js
@@ -7,13 +7,55 @@
  */
 
 import React from 'react';
+import { Image, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from 'cryptotracker/src/components/coins/CoinsStack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from 'cryptotracker/src/res/colors.js';
+import FavouritesStack from 'cryptotracker/src/components/favourites/FavouritesStack';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: Colors.primary,
+          tabBarInactiveTintColor: Colors.text,
+          tabBarStyle: {
+            backgroundColor: Colors.backgroundAccent,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('cryptotracker/src/assets/bank.png')}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Favourites"
+          component={FavouritesStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('cryptotracker/src/assets/bank.png')}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+
     </NavigationContainer>
   );
 };
